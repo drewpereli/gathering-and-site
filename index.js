f97d66278cdce41e3abbb972f85ad5a8500b6b22
Drew Pereli <drewpereli@gmail.com>
2021-09-24 17:54:07 -0700

    Set up basic page and content

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 import './src/styles/app.css';
+
+document.querySelectorAll('.main-nav-item').forEach((el) => {
+  let sectionName = el.dataset.section;
+
+  el.addEventListener('click', () => {
+    let section = document.getElementById(`section-${sectionName}`);
+    section?.scrollIntoView({ behavior: 'smooth' });
+  });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+const imagesPath = '../images'; // Relative to ./src/styles/app.css
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'weed-1': `url('${imagesPath}/weed-1.jpg')`,
+        'weed-2': `url('${imagesPath}/weed-2.jpg')`,
+        'weed-3': `url('${imagesPath}/weed-3.jpg')`,
+        'weed-4': `url('${imagesPath}/weed-4.jpg')`,
+        'weed-5': `url('${imagesPath}/weed-5.jpg')`,
+      },
+      height: {
+        '50vh': '50vh',
+        '80vh': '80vh',
+      },
+    },
   },
   variants: {
     extend: {},
